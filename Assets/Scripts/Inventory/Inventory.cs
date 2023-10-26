8716632ae41ac99a5ab46a640256334f93c55dc2
Artem Anikin <108572282+Nicebear35@users.noreply.github.com>
2023-10-27 00:49:24 +0300

    0.2
    Adding Inventory opening coroutine

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,29 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    [SerializeField] private Button _inventoryButton;
+    [SerializeField] private RectTransform _inventoryButtonPosition;
+    [SerializeField] private float _timeToMove;
 
     private bool _isOpened;
+    private Vector3 _targetPosition;
+    private Vector3 _targetScale;
+    private Coroutine _openInventoryCoroutine;
 
     private void Start()
     {
         _isOpened = false;
-        transform.position = _inventoryButton.transform.position;
+        _targetPosition = _isOpened ? _inventoryButtonPosition.localPosition : Vector3.zero;
+        _targetScale = _isOpened ? Vector3.zero : Vector3.one;
+        transform.position = _inventoryButtonPosition.transform.position;
         transform.localScale = Vector3.zero;
     }
 
-    public IEnumerator ShowOrHideInventory(Vector3 position, Vector3 scale)
+    private IEnumerator ShowOrHideInventory(Vector3 targetPosition, Vector3 targetScale)
     {
-        while (transform.localScale != position && transform.position != scale)
+        while (transform.localPosition != targetPosition)
         {
-            // тут будет корутина сворачивания и разворачивания инвентаря 
-            // он будет прятаться за кнопку и уменьшаться до нуля
+            Debug.Log($"current position - {transform.localPosition} / target - {targetPosition}");
+
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPosition, _timeToMove);
+            transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, _timeToMove);
             yield return null;
+
+            Debug.Log($"position - {transform.localPosition} / scale - {transform.localScale}");
         }
 
+        _isOpened = !_isOpened;
+    }
+
+    public void OpenOrCloseInventory()
+    {
+        if (_openInventoryCoroutine == null)
+        {
+            _openInventoryCoroutine = StartCoroutine(ShowOrHideInventory(_targetPosition, _targetScale));
+        }
+        else
+        {
+            StopCoroutine(_openInventoryCoroutine);
+        }
     }
 }
